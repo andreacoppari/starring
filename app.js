b64510cd9411a0867733bc130b7c79de6a373649
andreacoppari <andrea.coppari@proton.me>
2022-05-17 23:10:58 +0200

    feat(homepage): connection to db and router

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
+const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
+const exphbs = require('express-handlebars')
+const connectDB = require('./config/db')
 
 //load
 dotenv.config({ path: './config/config.env' })
 
+connectDB()
+
 const app = express()
+
+// logging
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan('dev'))
+}
+// handlebars
+app.engine('.hbs', exphbs.engine({defaultLayout: 'main', extname: ".hbs"}))
+app.set('view engine', '.hbs')
+
+// static
+app.use(express.static(path.join(__dirname, 'public')))
+
+// routes
+app.use('/', require('./routes/index'))
 
 const PORT = process.env.PORT || 8000
 
---
+++ routes/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express.Router()
+
+// @desc    Login/Landing page
+// @route   GET /
+router.get('/', (req, res) => {
+    res.render('homepage')
+})
+
+// @desc    Homepage
+// @route   GET /home
+router.get('/login', (req, res) => {
+    res.render('login',
+    {layout: 'login'})
+})
+
+module.exports = router

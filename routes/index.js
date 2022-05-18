6c66ad9b288d5b52b6fb81d6bff892b4e6099c09
Tobias Carcereri <95741642+Tobic0@users.noreply.github.com>
2022-05-18 18:48:13 +0200

    added get for /film for search bar

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,18 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose');
 
 // @desc    Login/Landing page
 // @route   GET /
 router.get('/', (req, res) => {
-    res.render('homepage')
+    mongoose.connection.db.collection('films').find().toArray()
+    .then(results => {
+        res.render('homepage', {test: results})
+    })
 })
 
-// @desc    Homepage
-// @route   GET /home
+// @desc    Login
+// @route   GET /login
 router.get('/login', (req, res) => {
     res.render('login',
     {layout: 'login'})
@@ -19,6 +23,16 @@ router.get('/login', (req, res) => {
 router.get('/signup', (req, res) => {
     res.render('signup',
     {layout: 'signup'})
+})
+
+// @desc    Film
+// @route   GET /film
+router.get('/film', (req, res) => {
+    console.log("log: "+req.query.search)
+    mongoose.connection.db.collection('films').find({"title": new RegExp('.*' + req.query.search + '.*')}).toArray()
+    .then(results => {
+        console.log(results)
+    })
 })
 
 module.exports = router

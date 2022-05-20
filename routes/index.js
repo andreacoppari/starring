9c615ad974e36628d7281ddd565a9b7ab2aaaa14
Tobias Carcereri <95741642+Tobic0@users.noreply.github.com>
2022-05-20 23:25:48 +0200

    fixed type and replaced /film with /search

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 // @route   GET /
 router.get('/', (req, res) => {
     Promise.all([
-        mongoose.connection.db.collection('movies').find().limit(10).sort({'Staeeing rating': -1}).toArray(),
+        mongoose.connection.db.collection('movies').find().limit(10).sort({'Starring rating': -1}).toArray(),
         mongoose.connection.db.collection('movies').find().limit(10).sort({'year': -1}).toArray()
     ])
     .then(([film_recommended, film_new]) => {
@@ -30,11 +30,12 @@ router.get('/signup', (req, res) => {
 
 // @desc    Film
 // @route   GET /film
-router.get('/film', (req, res) => {
+router.get('/search', (req, res) => {
     console.log("log: "+req.query.search)
-    mongoose.connection.db.collection('films').find({"movies": new RegExp('.*' + req.query.search + '.*')}).toArray()
+    mongoose.connection.db.collection('movies').find({"title": new RegExp('.*' + req.query.search + '.*')}).toArray()
     .then(results => {
-        console.log(results)
+        console.log("res: "+results)
+        res.render('search', {content: results})
     })
 })
 
